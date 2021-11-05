9bc7f531ae526a267fa6517eb2ce07e53570bda4
LeidyMol <lady91molina@gmail.com>
2021-11-05 18:50:53 -0500

    se sube plantilla y avance de una pantalla

---
+++ inventario-frontend/src/components/edit-activos.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class EditActivos extends Component {
+ render() {
+ return (
+ <div>
+ <p>React Edit Activos Component!</p>
+ </div>
+ );
+ }
+}
